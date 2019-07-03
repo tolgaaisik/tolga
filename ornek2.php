c72d5fce0d823a3e02dc93d26273ca49d50359fd
tolgaaisik <52401166+tolgaaisik@users.noreply.github.com>
2019-07-03 10:47:36 +0300

    Add files via upload

---
+++ ornek2.php
@@ -0,0 +1,21 @@
+<?php
+$not=67;
+if ($not<=50) {
+	echo "1";
+}
+elseif ($not>50 && $not<=60) {
+	echo "2";
+}
+elseif ($not>60 && $not<=70) {
+	echo "3";
+}
+elseif ($not>70 && $not<=85) {
+	echo "4";
+}
+elseif ($not>85 && $not<=100) {
+	echo "5";
+}
+elseif ($not>100) {
+	echo "yanlış girdin tekrar gir";
+}
+?>
