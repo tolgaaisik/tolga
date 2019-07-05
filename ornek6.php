a5739df10f9d8a5609e37303c7ca2bb3f37fa028
tolgaaisik <52401166+tolgaaisik@users.noreply.github.com>
2019-07-05 15:22:35 +0300

    Add files via upload

---
+++ ornek6.php
@@ -0,0 +1,12 @@
+<?php
+for ($i=0; $i <=100 ; $i++) { 
+	if($i%2==0){
+echo $i. "<br>";
+	}
+	# code...
+	}
+	//Küçükten büyüğe merhaba dünya yazar
+	for ($i=6; $i>0 ; $i--) { 
+		 echo "<h".$i.">"."Merhaba Dünya"."</h".$i.">";
+		}
+	?>	
