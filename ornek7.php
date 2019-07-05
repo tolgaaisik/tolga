ed9fc6759bc9eee7c8b21cbd41781e1333ab46b4
tolgaaisik <52401166+tolgaaisik@users.noreply.github.com>
2019-07-05 15:24:31 +0300

    Add files via upload

---
+++ ornek7.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ornek4</title>
+</head>
+<body>
+<?php
+$cars=array
+ (
+ array("BMW",22,15),
+ array("mercedes",23,12),
+ array("ford",12,2)
+) ;
+echo $cars[0][0].":Stok:".$cars[0][1].":maliyet:".$cars[0][2]."<br>";
+echo $cars[1][0].":Stok:".$cars[1][1].":maliyet:".$cars[1][2]."<br>";
+echo $cars[2][0].":Stok:".$cars[2][1].":maliyet:".$cars[2][2]."<br>";
+
+
+
+
+
+?>
+
+
+ 
+
+
+	
+
+
+</body>
+</html>
